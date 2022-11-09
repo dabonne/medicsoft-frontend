da88c6e0210cddece4d54034cab1de6e7a8807ed
Sinali Dionou <sinaloid@gmail.com>
2022-11-09 17:25:37 +0000

    Procfile + server.js added

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+// eslint-disable-next-line no-unused-vars
+// const bodyParser = require('body-parser');
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.use(express.static(path.join(__dirname, "build")));
+
+// This route serves the React app
+app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
+
+app.listen(PORT, () => console.log(`Server listening on PORT ${PORT}`));
