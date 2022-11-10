f229f61e7302a05e40b67c228f5c7eebaa3a6f59
Sinali Dionou <sinaloid@gmail.com>
2022-11-10 15:12:52 +0000

    edit server.js

--- server.js
+++ server.js
@@ -8,6 +8,6 @@ const PORT = process.env.PORT || 8080;
 app.use(express.static(path.join(__dirname, "build")));
 
 // This route serves the React app
-app.get('/', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
+app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, "build", "index.html")));
 
 app.listen(PORT, () => console.log(`Server listening on PORT ${PORT}`));
